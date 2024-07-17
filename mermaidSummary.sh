318cf5544aeb78ad3506329b67544493058bf5db
chesspro13 <chesspro13@gmail.com>
2024-07-17 15:15:10 -0700

    Init

---
+++ mermaidSummary.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+hostname=$HOSTNAME
+ipaddress=$(ip addr | grep "eno" | grep -oP 'inet\s\K(\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})')
+os=$(lsb_release -ad 2>/dev/null | grep -i description | cut -s -f2)
+drives=$(lsblk -o NAME,SIZE,TYPE | grep disk)
+driveList=$(echo ${drives//disk/} | tr -d '\n')
+
+
+mem=$(grep MemTotal /proc/meminfo)
+ramKB=${mem//MemTotal:/}
+ramTotal=${ramKB//kB/}
+kb=$(echo $ramTotal | tr -d ' ')
+ram="$(($kb/(1024*1024)))GB"
+
+echo "Hostname: $hostname"
+echo "OS: $os" 
+echo "IP Address: $ipaddress"
+echo "Drives: $driveList"
+echo "Ram: $ram"
+
+echo "Mermaid Notation:"
+echo "$hostname[$hostname<br/>IP: $ipaddress<br/>OS: $os<br>Software: N/A<br/>HHD: $driveList<br/>Memory: $ram]:::device"
+
